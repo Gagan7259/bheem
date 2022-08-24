6239736330648b02d4455cd93b36a399633c6538
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-24 19:59:31 +0530

    6

---
+++ form/fatArrow/0.js
@@ -0,0 +1,14 @@
+var wish =() => {
+    return("good morning")
+}
+let r1=wish()
+    console.log(r1);
+
+
+
+    var wish = () => {
+        return "good night"
+    }
+    let t1=wish()
+    console.log(t1);
+
