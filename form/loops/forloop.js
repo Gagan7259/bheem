ae53bc05091363ee6dcbe4a10af38f88dc5322cb
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-25 10:52:41 +0530

    ok

--- form/loops/forloop.js
+++ form/loops/forloop.js
@@ -23,3 +23,12 @@ do{
 var numbers=[100,69,89,45]
 for( number of numbers)
 console.log(number);
+
+// deleting Array
+var cities=["banglore","kolkatta","chennai", "u"];
+cities.splice(3)
+cities.splice(2)
+cities.splice(1)
+cities.splice(0)
+cities.splice(-1)
+console.log(cities);
