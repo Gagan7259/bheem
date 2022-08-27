0c1183e3abbe56f861f049034adf05a67ca03498
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-27 09:30:28 +0530

    g

--- form/constructer/emp.js
+++ form/constructer/emp.js
@@ -15,17 +15,10 @@ class Employee{
     getname(){
         return this.name
     }
-    setsalary(salary){
-        this.salaray=salary;
-    }
-    getsalary(){
-        return this.salary
-    }
 }
 let g1=new Employee
 g1.setid(23);
 g1.setname("moumika");
-g1.salary(45000)
 console.log(g1);
 console.log(g1.getid());
 console.log(g1.getname());
