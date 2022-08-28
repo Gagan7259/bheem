1dcf3089ac8998f3e27f57f361b6221bd20a7f7c
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-08-28 10:49:13 +0530

    y

--- form/g/8.js
+++ form/g/8.js
@@ -2,3 +2,11 @@
 let gagan = 'welcome to chintamqni'
 len = gagan.length
 console.log(len)
+
+//accessing object properties
+let employee={
+    name:"Gagan",
+    sal:4000,
+}
+console.log(employee["name"]);
+console.log(employee["sal"]);
